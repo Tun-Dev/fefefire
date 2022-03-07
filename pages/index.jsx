d3a7a5bcfe779eda1bb15e47747a71232d164dc8
Douye Tungbulu <tungbulupaul@gmail.com>
2022-03-08 00:00:52 +0100

    Styling

--- pages/index.jsx
+++ pages/index.jsx
@@ -10,6 +10,9 @@ import Cont32 from "../public/cont32.png";
 import Cont4 from "../public/cont4.png";
 import Cont52 from "../public/cont52.png";
 import Cont53 from "../public/cont53.png";
+import Cont61 from "../public/cont61.png";
+import Cont62 from "../public/cont62.png";
+import Cont7 from "../public/cont7.png";
 
 export default function Home() {
   return (
@@ -26,42 +29,42 @@ export default function Home() {
       </div>
       {/* Container 2 */}
       <div className={styles.cont2}>
-        <div>
+        <div className={styles.cont2left}>
           <Image src={Cont2} />
         </div>
-        <div>
-          <h1>FEFIRE:Statment for days</h1>
+        <div className={styles.cont2right}>
+          <h1>FEFIRE: Statment for days</h1>
           <div>
-            <span></span>
+            <div className={styles.span}></div>
             <p>
               Lorem ipsum dolor sit amet, consectetur adipiscing elit.
               Ullamcorper cursus sagittis aliquam sed mattis. Rhoncus purus
               molestie rhoncus tellus.Lorem ipsum Ullamcorper cursus sagittis
               aliquam sed mattis.
             </p>
-            <span></span>
+            <div className={styles.span}></div>
           </div>
         </div>
       </div>
       {/* Container 3 */}
       <div className={styles.cont3}>
-        <div>
+        <div className={styles.cont31}>
           <div>
             <Image src={Cont31} />
           </div>
-          <div>
+          <div className={styles.cont3bot}>
             <h4>Gray Polly</h4>
-            <span></span>
+            <div className={styles.span2}></div>
             <p>$200.9</p>
           </div>
         </div>
-        <div>
+        <div className={styles.cont32}>
           <div>
             <Image src={Cont32} />
           </div>
-          <div>
+          <div className={styles.cont3bot}>
             <h4>Red Berry</h4>
-            <span></span>
+            <div className={styles.span2}></div>
             <p>$200.9</p>
           </div>
         </div>
@@ -81,6 +84,7 @@ export default function Home() {
           <Image src={Cont4} />
         </div>
       </div>
+      {/* Container 5 */}
       <div className={styles.cont5}>
         <div>
           <div>
@@ -112,6 +116,18 @@ export default function Home() {
             <p>$200.9</p>
           </div>
         </div>
+      </div>
+      <div className={styles.cont6}>
+        <h1>The Cotton Quality✨</h1>
+        <div>
+          <Image src={Cont61} />
+          <Image src={Cont62} />
+        </div>
+      </div>
+      {/* Container 7 */}
+      <div className={styles.cont7}>
+        <h1>All New Arrivals🛬</h1>
+        <Image src={Cont7} />
       </div>
     </div>
   );
