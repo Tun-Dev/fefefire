0fed38ce8dbdb760feecf8b6f771030571f90056
Douye Tungbulu <tungbulupaul@gmail.com>
2022-03-16 21:05:01 +0100

    Footer

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -4,32 +4,58 @@ import { useEffect, useRef } from "react";
 import { gsap } from "gsap/dist/gsap";
 
 const Footer = () => {
-  let footer = useRef(null);
-  let footer1 = useRef(null);
-  var tl = gsap.timeline();
-  // For Footer
-  useEffect(() => {
-    gsap.fromTo(
-      [footer, footer1],
-      { y: -10 },
-      {
-        y: 15,
-        repeat: -1,
-        yoyo: true,
-        ease: "power1.inOut",
-        duration: 1,
-      }
-    );
-  });
+  // let footer = useRef(null);
+  // let footer1 = useRef(null);
+  // var tl = gsap.timeline();
+  // // For Footer
+  // useEffect(() => {
+  //   gsap.fromTo(
+  //     [footer, footer1],
+  //     { y: -10 },
+  //     {
+  //       y: 15,
+  //       repeat: -1,
+  //       yoyo: true,
+  //       ease: "power1.inOut",
+  //       duration: 1,
+  //     }
+  //   );
+  // });
+  // ref={(el) => (footer = el)}
+  // ref={(el) => (footer1 = el)}
 
   return (
     <footer>
-      <div className="fticons" ref={(el) => (footer = el)}>
+      <div className="fticons">
         <Image src={FB} />
         <Image src={Twitter} />
         <Image src={IG} />
       </div>
-      <div className="ftwords" ref={(el) => (footer1 = el)}>
+      {/* <div>
+        <div>
+          <h1>
+            Designed by{" "}
+            <a
+              href="https://oghenetega.disha.page/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Oghenetega
+            </a>
+          </h1>
+          <h1>
+            Developed by{" "}
+            <a
+              href="http://twitter.com/tunzdev"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              ZOD.jsx
+            </a>
+          </h1>
+        </div>
+      </div> */}
+      <div className="ftwords">
         <p>reachfefefire@enquries.com</p>
         <Image src={Fefelogo} />
       </div>
